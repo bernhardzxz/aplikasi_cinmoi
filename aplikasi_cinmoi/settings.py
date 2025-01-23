ff68119a8fee624cd2a706d1cabe49350788410a
dermawan <dermawan.bernhard09@gmail.com>
2025-01-23 18:35:10 +0700

    update data
    update templates dll

--- aplikasi_cinmoi/settings.py
+++ aplikasi_cinmoi/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'aplikasi_cinmoi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates", "cinmoi_app/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+LOGIN_REDIRECT_URL = '/dashboard'
+LOGIN_URL = '/login'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
