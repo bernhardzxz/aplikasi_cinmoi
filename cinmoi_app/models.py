d7d19d889184d882a08c6149b006ab31d509ba2d
dermawan <dermawan.bernhard09@gmail.com>
2025-02-04 12:34:34 +0700

    fix
    update

--- cinmoi_app/models.py
+++ cinmoi_app/models.py
@@ -1,20 +1,66 @@
+# from django.db import models
+# from django.contrib.auth.models import User
+
+# class Menu(models.Model):
+#     # Definisikan pilihan untuk tipe rasa
+#     TIPE_RASA_CHOICES = [
+#         ('manis', 'Manis'),
+#         ('gurih', 'Gurih'),
+#         ('pedas', 'Pedas'),
+#     ]
+
+#     # Definisikan pilihan untuk kategori
+#     KATEGORI_CHOICES = [
+#         ('appetizer', 'Appetizer'),
+#         ('beverage', 'Beverage'),
+#         ('congee', 'Congee'),
+#         ('dessert', 'Dessert'),
+#     ]
+    
+#     nama = models.CharField(max_length=100)
+#     deskripsi = models.TextField()
+#     gambar = models.ImageField(upload_to='menu_images/')
+#     kategori = models.CharField(max_length=50, choices=KATEGORI_CHOICES)  # Menggunakan choices untuk kategori
+#     tipe_rasa = models.CharField(max_length=50, choices=TIPE_RASA_CHOICES)  # Menggunakan choices untuk tipe rasa
+#     class Menu(models.Model):TIPE_RASA_CHOICES = [
+#         ('manis', 'Manis'),
+#         ('gurih', 'Gurih'),
+#         ('pedas', 'Pedas'),
+#     ]
+
+#     KATEGORI_CHOICES = [
+#         ('appetizer', 'Appetizer'),
+#         ('beverage', 'Beverage'),
+#         ('congee', 'Congee'),
+#         ('dessert', 'Dessert'),
+#     ]
+    
+#     nama = models.CharField(max_length=100)
+#     deskripsi = models.TextField()
+#     gambar = models.ImageField(upload_to='menu_images/')
+#     kategori = models.CharField(max_length=100,null=True, blank=True, choices=KATEGORI_CHOICES)
+#     tipe_rasa = models.CharField(max_length=100,null=True, blank=True, choices=TIPE_RASA_CHOICES)
+
+#     def __str__(self):
+#         return self.nama
+
+
 from django.db import models
-from django.contrib.auth.models import User
 
 class Menu(models.Model):
     # Definisikan pilihan untuk tipe rasa
     TIPE_RASA_CHOICES = [
         ('manis', 'Manis'),
-        ('asin', 'Asin'),
+        ('gurih', 'Gurih'),
         ('pedas', 'Pedas'),
     ]
 
     # Definisikan pilihan untuk kategori
     KATEGORI_CHOICES = [
-        ('main_course', 'Main Course'),
-        ('side_dish', 'Side Dish'),
-        ('dessert', 'Dessert'),
+        ('appetizer', 'Appetizer'),
         ('beverage', 'Beverage'),
+        ('congee', 'Congee'),
+        ('dessert', 'Dessert'),
     ]
     
     nama = models.CharField(max_length=100)
@@ -22,26 +68,7 @@ class Menu(models.Model):
     gambar = models.ImageField(upload_to='menu_images/')
     kategori = models.CharField(max_length=50, choices=KATEGORI_CHOICES)  # Menggunakan choices untuk kategori
     tipe_rasa = models.CharField(max_length=50, choices=TIPE_RASA_CHOICES)  # Menggunakan choices untuk tipe rasa
-    class Menu(models.Model):TIPE_RASA_CHOICES = [
-        ('manis', 'Manis'),
-        ('asin', 'Asin'),
-        ('pedas', 'Pedas'),
-    ]
-
-    KATEGORI_CHOICES = [
-        ('main_course', 'Main Course'),
-        ('side_dish', 'Side Dish'),
-        ('dessert', 'Dessert'),
-        ('beverage', 'Beverage'),
-    ]
-    
-    nama = models.CharField(max_length=100)
-    deskripsi = models.TextField()
-    gambar = models.ImageField(upload_to='menu_images/')
-    kategori = models.CharField(max_length=100,null=True, blank=True, choices=KATEGORI_CHOICES)
-    tipe_rasa = models.CharField(max_length=100,null=True, blank=True, choices=TIPE_RASA_CHOICES)
-    harga = models.IntegerField(null=True, blank=True)
+    harga = models.DecimalField(max_digits=10, decimal_places=2)  # Menambahkan field harga
 
     def __str__(self):
         return self.nama
-
